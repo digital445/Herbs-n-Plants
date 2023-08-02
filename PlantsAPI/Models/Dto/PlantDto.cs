1321e8921fa3be6e0068bce0e07b750143ae9ad3
Kasimov Aleksandr <digital445@hotmail.com>
2023-08-02 18:39:12 +0600

    Some fixes for Models and Dtos

--- PlantsAPI/Models/Dto/PlantDto.cs
+++ PlantsAPI/Models/Dto/PlantDto.cs
@@ -5,8 +5,8 @@ namespace Services.PlantsAPI.Models.Dto
 	public class PlantDto
 	{
         public int PlantId { get; set; }
-        public string? Name { get; set; }
-        public Color FlowerColor { get; set; } = Color.Green; //main color of a flower
+		public List<PlantNameDto>? Names { get; set; }
+		public Color FlowerColor { get; set; } = Color.Green; //main color of a flower
         public bool Poisonous { get; set; }
         public bool ForHerbalTea { get; set; }
         public bool PickingProhibited { get; set; } //if picking flowers is prohibited by law
--- PlantsAPI/Models/Dto/PlantNameDto.cs
+++ PlantsAPI/Models/Dto/PlantNameDto.cs
@@ -7,7 +7,7 @@ namespace Services.PlantsAPI.Models.Dto
 	{
         public int PlantNameId { get; set; }
 		public int PlantId { get; set; }
-		public virtual Plant? Plant { get; set; }
+		public virtual PlantDto? Plant { get; set; }
 		public string? Name { get; set; }	
 	}
 }
